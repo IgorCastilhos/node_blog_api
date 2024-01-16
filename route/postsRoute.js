cdc6dcb62aef331c011f8b83ad0d339b1e3df5c6
igorc <igorcastilhos2010@hotmail.com>
2024-01-16 16:07:21 -0300

    Functional [x]. Adicionando o resto da API

--- route/postsRoute.js
+++ route/postsRoute.js
@@ -1,23 +1,32 @@
-const express = require("express")
+const express = require("express");
 // Invoca o roteador que possibilita distribuir as rotas
 const router = express.Router();
 const postsService = require("../service/postsService");
 
-router.get("/posts", async function(req,res) {
+// GET Route - Recupera todos os posts
+router.get("/posts", async function (req, res) {
   const posts = await postsService.getPosts();
   res.json(posts);
 });
-router.get("/posts/:id", async function(req,res) {
 
+// POST Route - Cria um post
+router.post("/posts", async function (req, res) {
+  const post = req.body;
+  const newPost = await postsService.savePost(post);
+  res.json(newPost);
 });
-router.post("/posts", async function(req,res) {
 
+// PUT Route - Atualiza um post conforme o seu id
+router.put("/posts/:id", async function (req, res) {
+  const post = req.body;
+  await postsService.updatePost(req.params.id, post);
+  res.end()
 });
-router.put("/posts/:id", async function(req,res) {
 
-});
-router.delete("/posts/:id", async function(req,res) {
-
+// DELETE Route -
+router.delete("/posts/:id", async function (req, res) {
+  await postsService.deletePost(req.params.id);
+  res.end()
 });
 
 module.exports = router;
--- test/posts.test.js
+++ test/posts.test.js
@@ -1,13 +1,63 @@
-const axios = require("axios")
+const axios = require("axios");
+const crypto = require("crypto");
+const postsService = require("../service/postsService");
 
-test('Should get posts', async function(){
-  const response = await axios({
-    url: "http://localhost:3000/posts",
-    method: "GET"
-  })
+const generate = function () {
+  return crypto.randomBytes(20).toString('hex');
+};
+
+const request = function (url, method, data) {
+  return axios({url, method, data});
+};
+
+// GET TEST
+test("Should get posts", async function () {
+  // given - dado que
+  const post1 = await postsService.savePost({title: generate(), content: generate()});
+  const post2 = await postsService.savePost({title: generate(), content: generate()});
+  const post3 = await postsService.savePost({title: generate(), content: generate()});
+  // when = quando acontecer
+  const response = await request("http://localhost:3000/posts", "get")
   const posts = response.data;
+  // then - então
   expect(posts).toHaveLength(3);
-  const [firstPost] = posts;
-  expect(firstPost.id).toBe(1);
-  expect(firstPost.title).toBe("REST API: Métodos")
-})
+  await postsService.deletePost(post1.id);
+  await postsService.deletePost(post2.id);
+  await postsService.deletePost(post3.id);
+});
+
+// SAVE TEST
+test("Should save post", async function () {
+  const data = { title: generate(), content: generate()};
+  const response = await request("http://localhost:3000/posts", "post", data)
+  const post = response.data;
+  expect(post.title).toBe(data.title);
+  expect(post.content).toBe(data.content);
+  await postsService.deletePost(post.id);
+});
+
+// UPDATE TEST
+test("Should update post", async function () {
+  const post = await postsService.savePost({ title: generate(), content: generate()});
+
+  post.title = generate();
+  post.content = generate();
+
+  await request(`http://localhost:3000/posts/${post.id}`, "put", post)
+  const updatedPost = await postsService.getPost(post.id);
+
+  expect(updatedPost.title).toBe(post.title);
+  expect(updatedPost.content).toBe(post.content);
+
+  await postsService.deletePost(post.id);
+});
+
+// DELETE TEST
+test("Should delete a post", async function () {
+  const post = await postsService.savePost({ title: generate(), content: generate() });
+
+  await request(`http://localhost:3000/posts/${post.id}`, "delete")
+
+  const posts = await postsService.getPosts();
+  expect(posts).toHaveLength(0);
+});
